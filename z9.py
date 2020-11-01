ead33e26fb7eb95231f7f559e1708bd7fff6f575
nnT_NJe0n9 <33746392+nnT-Nje0n9@users.noreply.github.com>
2020-11-01 17:26:05 +0900

    Update z9.py

--- z9.py
+++ z9.py
@@ -1,20 +1,19 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+
 #Southern Oscillation Index (SOI)
 fig = plt.figure(figsize=(20,12))
-df = pd.read_csv('C:/Users/user/Desktop/test.csv')
+df = pd.read_csv('C:/Users/lolgo/OneDrive/바탕 화면/test.csv',sep=',')
 
-
-#df['Date'][0:2] = pd.to_datetime(df['Date'][0:2])
-
+df['Date'] = pd.to_datetime(df['Date'])
 
 ax = fig.add_subplot(1,1,1)
 ax.set(title = 'Southern Oscillation Index (SOI)')
 x_values = df['Date']
 y_values = df['Value']
-#plt.plot(x_values,y_values)
-#plt.show()
+
 ax.plot(x_values,y_values,linewidth = 1) 
 plt.axhline(y=0,color='r')
 plt.show()
